6cb1da0ee098fe3fa6b194d5dc1142d990fec17f
Shashank Kushwaha <kushwaha1@llnl.gov>
2026-04-01 21:47:55 -0700

    feat: Add RSA settings to RunSettings dataclass

--- lc_conductor/backend_helper_function.py
+++ lc_conductor/backend_helper_function.py
@@ -7,6 +7,13 @@ from pydantic import Field
 class RunSettings:
     prompt_debugging: bool = Field(alias="promptDebugging", default=False)
 
+    # RSA settings
+    use_rsa: bool = Field(alias="useRsa", default=False)
+    rsa_mode: str = Field(alias="rsaMode", default="standalone")
+    rsa_n: int = Field(alias="rsaN", default=8)
+    rsa_k: int = Field(alias="rsaK", default=4)
+    rsa_t: int = Field(alias="rsaT", default=3)
+
 
 async def loop_executor(executor, func, *args, **kwargs):
     loop = asyncio.get_event_loop()
